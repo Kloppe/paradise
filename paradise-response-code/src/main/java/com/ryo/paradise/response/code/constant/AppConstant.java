3cbc5977619794a0270f2b49878ffec24b0ae20b
bbhou <1060732496@qq.com>
2017-09-30 11:34:42 +0800

    1. 响应码。

---
+++ paradise-response-code/src/main/java/com/ryo/paradise/response/code/constant/AppConstant.java
@@ -0,0 +1,16 @@
+package com.ryo.paradise.response.code.constant;
+
+/**
+ * 系统常量
+ * Created by bbhou on 2017/9/30.
+ * @author bbhou
+ * @version 1.0.0
+ */
+public class AppConstant {
+
+    /**
+     * 系统编码
+     */
+    public static final String SYSTEM_CODE = "001";
+
+}
